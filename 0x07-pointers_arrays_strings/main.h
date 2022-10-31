4a06b87cab099847ba4c783fbbe45aa569bb1fb0
fawaz27 <boukarifawaz27@gmail.com>
2022-10-31 21:49:26 +0100

    last commit

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * File: main.h
+ * Auth: Fawaz
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x07-pointers_arrays_strings directory.
+ */
+
+#include<stddef.h>
+
+char * _memset(char *s, char c, unsigned int n);
+
+#endif
