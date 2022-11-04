29775b9fd6c009a3a83aa829019b13556c91e9ba
fawaz27 <boukarifawaz27@gmail.com>
2022-11-04 23:43:41 +0100

    last

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - a program that prints all arguments it receives.
+* @argc: a count of the arguments supplied to the program.
+* @argv: array of pointers to the strings which are those arguments.
+* Return: 0 - if success.
+*/
+
+int main(int argc, char *argv[])
+{
+	int i = 0;
+	while (i < argc)
+	{
+		printf("%s\n", argv[i]);
+		i++;
+	}
+	return (0);
+}
