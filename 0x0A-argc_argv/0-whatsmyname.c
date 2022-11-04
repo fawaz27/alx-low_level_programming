2f205461cfdf5ef3a44b70bc10e59b1391b3c96a
fawaz27 <boukarifawaz27@gmail.com>
2022-11-04 23:25:50 +0100

    last commit

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,9 +6,9 @@
 * Return: 0 - if success.
 */
 
-int main(int argc __attribute__((unused)), char *argv[])
+int main(int argc, char *argv[])
 {
-	
-	printf("%s\n", argv[0]);
+	if (argc >= 0)
+		printf("%s\n", argv[0]);
 	return (0);
 }
